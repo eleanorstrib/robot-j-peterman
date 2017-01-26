8335010bc821c3a503353587ebb4905efefa72a9
eleanorstrib <hello@eleanorstrib.com>
2017-01-25 20:58:01 -0800

    reverted to a previous version, gets urls

--- web_scraper.py
+++ web_scraper.py
@@ -3,11 +3,23 @@ from womens_depts import womens_depts
 from urllib.request import urlopen
 
 def get_womens_data():
+    soup_list = []
     for item in womens_depts:
         url = "http://www.jpeterman.com/Womens-%s?by=Departments"%item
         web_page = urlopen(url)
         soup = bsoup(web_page.read())
+        soup_list.append(soup)
+    return soup_list
 
-        print(soup)
+def get_all_links(soup_list):
+    all_urls =[]
+    for item in soup_list:
+        links = item.select('.prlst-itm-detail > a')
+        for item in links:
+            all_urls.append(item['href'])
+    return all_urls
 
-get_womens_data()
+
+if __name__  ==  "__main__":
+    list_page_soup = get_womens_data()
+    print(get_all_links(list_page_soup))
--- womens_depts.py
+++ womens_depts.py
@@ -1,6 +1,5 @@
-womens_depts = ['Dresses', 'Caftans', 'Blouses', 'Sweaters', 'Skirts', 'Pants',
-                'Blazers-and-Jackets', 'Outerwear', 'Sleepwear'
- ]
+# womens_depts = ['Dresses', 'Caftans', 'Blouses', 'Sweaters', 'Skirts', 'Pants',
+#                 'Blazers-and-Jackets', 'Outerwear', 'Sleepwear'
+#  ]
 
-for item in womens_depts:
-    print(item)
+womens_depts = ['Dresses', 'Caftans', 'Blouses', 'Sweaters']
